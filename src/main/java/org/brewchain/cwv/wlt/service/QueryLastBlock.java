d2485c5105999333ada2cb97efd668de4c889157
dumingyue <dumy@finchain60.com>
2018-08-20 15:05:16 +0800

    增加查询块高度接口

---
+++ src/main/java/org/brewchain/cwv/wlt/service/QueryLastBlock.java
@@ -0,0 +1,59 @@
+package org.brewchain.cwv.wlt.service;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.brewchain.cwv.wlt.dao.Daos;
+import org.brewchain.cwv.wlt.dbgens.wlt.entity.CWVWltParameter;
+import org.brewchain.cwv.wlt.dbgens.wlt.entity.CWVWltParameterExample;
+import org.brewchain.cwv.wlt.helper.TransactionHelper;
+import org.brewchain.cwv.wlt.utils.DESedeCoder;
+import org.brewchain.wallet.service.Wallet.BaseData;
+import org.brewchain.wallet.service.Wallet.PWLTCommand;
+import org.brewchain.wallet.service.Wallet.PWLTModule;
+import org.brewchain.wallet.service.Wallet.ReqGetTxByHash;
+import org.brewchain.wallet.service.Wallet.RespBlockDetail;
+import org.brewchain.wallet.service.Wallet.RespGetTxByHash;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import onight.oapi.scala.commons.SessionModules;
+import onight.osgi.annotation.NActorProvider;
+import onight.tfw.async.CompleteHandler;
+import onight.tfw.ntrans.api.annotation.ActorRequire;
+import onight.tfw.otransio.api.PacketHelper;
+import onight.tfw.otransio.api.beans.FramePacket;
+import onight.tfw.outils.bean.JsonPBFormat;
+
+@NActorProvider
+@Slf4j
+@Data
+public class QueryLastBlock extends SessionModules<BaseData>{
+
+	@ActorRequire(name = "txHelper", scope = "global")
+	TransactionHelper txHelper;
+	
+	@ActorRequire(name = "daos", scope = "global")
+	Daos daos;
+	
+	@Override
+	public String[] getCmds() {
+		return new String[] { PWLTCommand.GLB.name() };
+	}
+
+	@Override
+	public String getModule() {
+		return PWLTModule.WLT.name();
+	}
+
+	@Override
+	public void onPBPacket(final FramePacket pack, final BaseData pb, final CompleteHandler handler) {
+		RespBlockDetail.Builder ret = null;
+		ret = txHelper.queryLastBlock();
+		handler.onFinished(PacketHelper.toPBReturn(pack, ret.build()));
+	}
+}
