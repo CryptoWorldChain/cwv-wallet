33ce7ade9cdf09497745a2b3b95a4a017a170f65
dumingyue <dumy@finchain60.com>
2018-08-20 15:04:04 +0800

    增加房产交易

--- src/main/java/org/brewchain/cwv/wlt/enums/TransTypeEnum.java
+++ src/main/java/org/brewchain/cwv/wlt/enums/TransTypeEnum.java
@@ -3,7 +3,7 @@ package org.brewchain.cwv.wlt.enums;
 public enum TransTypeEnum {
 	TYPE_DEFAULT(0), TYPE_CreateUnionAccount(1), TYPE_TokenTransaction(2), TYPE_UnionAccountTransaction(
 			3), TYPE_CallInternalFunction(4), TYPE_CryptoTokenTransaction(5), TYPE_LockTokenTransaction(
-					6), TYPE_CreateContract(7), TYPE_CallContract(8), TYPE_CreateToken(9);
+					6), TYPE_CreateContract(7), TYPE_CallContract(8), TYPE_CreateToken(9), TYPE_CreateCryptoToken(10);
 	private int value = 0;
 
 	private TransTypeEnum(int value) {
